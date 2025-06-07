d63c42cb4367e3936a403e9db8cc52617d7a8c18
Giang <tenemva@gmail.com>
2025-06-07 11:45:29 +0700

    Cập nhâtj đường dẫn và điều hướng ngôn ngữ

--- docs/script.js
+++ docs/script.js
@@ -4,3 +4,13 @@ function toggleToc() {
   const toc = document.querySelector('.toc');
   toc.classList.toggle('collapsed');
 }
+function preventSameLang(lang) {
+  const path = window.location.pathname;
+
+  if ((lang === 'en' && path.includes('/en/')) ||
+      (lang === 'vi' && !path.includes('/en/'))) {
+    return false; // chặn chuyển hướng nếu đã ở ngôn ngữ đó
+  }
+
+  return true; // cho phép chuyển hướng nếu khác ngôn ngữ
+}
